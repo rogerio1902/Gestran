af7516ce5bb56780bfe5195f0f82054f238032b2
ROGERIO APARECIDO CLEMENTE <111748122+rogerio1902@users.noreply.github.com>
2024-05-29 01:45:41 -0300

    Add files via upload

--- Banco/Venda.sql
+++ Banco/Venda.sql
@@ -1,1 +1,19 @@
 
+CREATE DATABASE Gestran COLLATE Latin1_General_100_CI_AI
+
+USE Gestran;
+
+SET DATEFORMAT dmy;
+
+CREATE TABLE Venda (
+  Id        INT NOT NULL IDENTITY,
+  DataVenda DATE NOT NULL,
+  Vendedor  VARCHAR(12) NOT NULL,
+  Valor     MONEY NOT NULL,
+  Descto    MONEY,
+  Total     NUMERIC(19, 15) NOT NULL
+  CONSTRAINT PK_Venda PRIMARY KEY(Id)
+);
+
+CREATE INDEX IX_VendaData     ON Venda(DataVenda);
+CREATE INDEX IX_VendaVendedor ON Venda(DataVenda);
